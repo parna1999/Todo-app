c76351c9a9f656b845e5004f71cc65aef498db70
Parna <49149576+parna1999@users.noreply.github.com>
2021-06-15 11:23:18 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import TodoForm from './Components/TodoForm';
+import TodoList from './Components/TodoList';
+
+function App() {
+  return (
+    <div className="todoapp">
+      
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
